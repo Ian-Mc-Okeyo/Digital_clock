5b64d9d0141fc95b39ecc5bacd799e1bc22dda81
Ian-Mc-Okeyo <ianokeyo14559@gmail.com>
2021-11-16 15:41:45 +0300

    added a welcome message

--- digital_clock.py
+++ digital_clock.py
@@ -1,4 +1,7 @@
-from tkinter import Tk
+from tkinter import *
 root = Tk()
 root.geometry("600x300")
+root.config(bg="steelblue")
+label = Label(root, text="Welcome", font=("Arial Black", 78, "bold"), bg="steelblue", fg="white")
+label.pack(pady=100)
 root.mainloop()
